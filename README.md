2d5f6986a05ef0cc1f0b46c689b81151e2884a8e
jetsonhacks <jim@jetsonhacks.com>
2020-01-07 11:23:57 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # installROS
-install ROS Melodic on NVIDIA Jetson Development Kits
+Install Robot Operating System (ROS) on NVIDIA Jetson Developer Kits
+
+These scripts will install Robot Operating System (ROS) on the NVIDIA Jetson Developer Kits. 
+
+Jetson TX2, Jetson AGX Xavier, Jetson TX1, Jetson Nano
+
+The script is based on the Ubuntu ARM install of ROS Melodic: http://wiki.ros.org/melodic/Installation/Ubuntu
+
+Maintainer of ARM builds for ROS is http://answers.ros.org/users/1034/ahendrix/
+
+There are two scripts:
+
+<strong>installROS.sh</strong>
+<pre>
+Usage: ./installROS.sh  [[-p package] | [-h]]
+ -p | --package &lt;packagename&gt;  ROS package to install
+                               Multiple Usage allowed
+                               The first package should be a base package. One of the following:
+                                 ros-melodic-ros-base
+                                 ros-melodic-desktop
+                                 ros-melodic-desktop-full
+ </pre>
+ 
+Default is ros-melodic-ros-base if no packages are specified.
+
+Example Usage:
+
+$ ./installROS.sh -p ros-melodic-desktop -p ros-melodic-rgbd-launch
+
+This script installs a baseline ROS environment. There are several tasks:
+
+<ul>
+<li>Enable repositories universe, multiverse, and restricted</li>
+<li>Adds the ROS sources list</li>
+<li>Sets the needed keys</li>
+<li>Loads specified ROS packages, defaults to ros-melodic-base-ros if none specified</li>
+<li>Initializes rosdep</li>
+</ul>
+
+You can edit this file to add the ROS packages for your application. 
+
+<strong>setupCatkinWorkspace.sh</strong>
+Usage:
+
+$ ./setupCatkinWorkspace.sh [optionalWorkspaceName]
+
+where optionalWorkspaceName is the name of the workspace to be used. The default workspace name is catkin_ws. This script also sets up some ROS environment variables. Refer to the script for details.
+
+<em><b>Note:</b> On June 7, 2019 the GPG key for ROS was changed due to security issues. If you have ROS installed on your system before this, you should delete the GPG key:</em>
+ 
+<pre>
+$ sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116
+</pre> 
+
+
+## Release Notes
+<strong>January 2020</strong>
+* vL4T32.3.1
+* L4T 32.3.1 (JetPack 4.3)
+
+## License
+MIT License
+
+Copyright (c) 2017-2020 JetsonHacks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ 
---
+++ installROS.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+# Install Robot Operating System (ROS) on NVIDIA Jetson Developer Kit
+# Maintainer of ARM builds for ROS is http://answers.ros.org/users/1034/ahendrix/
+# Information from:
+# http://wiki.ros.org/melodic/Installation/UbuntuARM
+
+# Red is 1
+# Green is 2
+# Reset is sgr0
+
+function usage
+{
+    echo "Usage: ./installROS.sh [[-p package] | [-h]]"
+    echo "Install ROS Melodic"
+    echo "Installs ros-melodic-ros-base as default base package; Use -p to override"
+    echo "-p | --package <packagename>  ROS package to install"
+    echo "                              Multiple usage allowed"
+    echo "                              Must include one of the following:"
+    echo "                               ros-melodic-ros-base"
+    echo "                               ros-melodic-desktop"
+    echo "                               ros-melodic-desktop-full"
+    echo "-h | --help  This message"
+}
+
+function shouldInstallPackages
+{
+    tput setaf 1
+    echo "Your package list did not include a recommended base package"
+    tput sgr0 
+    echo "Please include one of the following:"
+    echo "   ros-melodic-ros-base"
+    echo "   ros-melodic-desktop"
+    echo "   ros-melodic-desktop-full"
+    echo ""
+    echo "ROS not installed"
+}
+
+# Iterate through command line inputs
+packages=()
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --package )        shift
+                                packages+=("$1")
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+# Check to see if other packages were specified
+# If not, set the default base package
+if [ ${#packages[@]}  -eq 0 ] ; then
+ packages+="ros-melodic-ros-base"
+fi
+echo "Packages to install: "${packages[@]}
+# Check to see if we have a ROS base kinda thingie
+hasBasePackage=false
+for package in "${packages[@]}"; do
+  if [[ $package == "ros-melodic-ros-base" ]]; then
+     hasBasePackage=true
+     break
+  elif [[ $package == "ros-melodic-desktop" ]]; then
+     hasBasePackage=true
+     break
+  elif [[ $package == "ros-melodic-desktop-full" ]]; then
+     hasBasePackage=true
+     break
+  fi
+done
+if [ $hasBasePackage == false ] ; then
+   shouldInstallPackages
+   exit 1
+fi
+
+# Let's start installing!
+
+tput setaf 2
+echo "Adding repository and source list"
+tput sgr0
+sudo apt-add-repository universe
+sudo apt-add-repository multiverse
+sudo apt-add-repository restricted
+
+# Setup sources.lst
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+# Setup keys
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+# If you experience issues connecting to the keyserver, you can try substituting hkp://pgp.mit.edu:80 or hkp://keyserver.ubuntu.com:80 in the previous command.
+# Installation
+tput setaf 2
+echo "Updating apt-get"
+tput sgr0
+sudo apt-get update
+tput setaf 2
+echo "Installing ROS"
+tput sgr0
+# This is where you might start to modify the packages being installed, i.e.
+# sudo apt-get install ros-melodic-desktop
+
+# Here we loop through any packages passed on the command line
+# Install packages ...
+for package in "${packages[@]}"; do
+  sudo apt-get install $package -y
+done
+
+# Add Individual Packages here
+# You can install a specific ROS package (replace underscores with dashes of the package name):
+# sudo apt-get install ros-melodic-PACKAGE
+# e.g.
+# sudo apt-get install ros-melodic-navigation
+#
+# To find available packages:
+# apt-cache search ros-melodic
+# 
+# Initialize rosdep
+tput setaf 2
+echo "Installing rosdep"
+tput sgr0
+sudo apt-get install python-rosdep -y
+# Certificates are messed up on earlier version Jetson for some reason
+# sudo c_rehash /etc/ssl/certs
+# Initialize rosdep
+tput setaf 2
+echo "Initializaing rosdep"
+tput sgr0
+sudo rosdep init
+# To find available packages, use:
+rosdep update
+# Environment Setup - Don't add /opt/ros/melodic/setup.bash if it's already in bashrc
+grep -q -F 'source /opt/ros/melodic/setup.bash' ~/.bashrc || echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+# Install rosinstall
+tput setaf 2
+echo "Installing rosinstall tools"
+tput sgr0
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+tput setaf 2
+echo "Installation complete!"
+echo "Please setup up your ROS network environment in ~/.bashrc"
+tput sgr0
+
